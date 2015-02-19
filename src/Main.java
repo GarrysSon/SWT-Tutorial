d1476b4c8c46093c6f9dfdebb23ea15de9573cd0
garrysSon <john.garrison@ou.edu>
2015-02-19 12:39:46 -0600

    Added listener to the button and changed it to say Exit.  Set the shell layout to RowLayout.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -5,12 +10,29 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		// The current users display.
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		
+		// The window.
 		shell.setText("SWT Demo");
 		shell.setSize(800, 800);
+		shell.setLayout(new RowLayout());
 		shell.open();
+		
+		// Push button.
+		Button button = new Button(shell, SWT.PUSH);
+		button.setText("Exit");
+		
+		// Assigning close operation to the button.
+		button.addSelectionListener(new SelectionAdapter()
+		{
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				shell.dispose();
+			}
+		});
 		
 		// While the shell is open.
 		while(!shell.isDisposed())
