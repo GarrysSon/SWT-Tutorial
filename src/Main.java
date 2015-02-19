62185157844e5e5cf2e42ba921e9bdf2cfe04481
garrysSon <john.garrison@ou.edu>
2015-02-19 12:28:19 -0600

    Created an empty window

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
-public class Main {
-
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		
+		shell.setText("SWT Demo");
+		shell.setSize(800, 800);
+		shell.open();
+		
+		// While the shell is open.
+		while(!shell.isDisposed())
+		{
+			// Wait until the next event, then process that event.
+			display.sleep();
+			display.readAndDispatch();
+		}
+	}
 }
